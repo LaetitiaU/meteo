dedd315c0ee3451addd8f9b77e0bfb4baef1d7a7
LaetitiaU <laetitia.usai@gmail.com>
2021-03-02 12:07:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const input = document.getElementById("villes");
+const button = document.getElementById("button");
+let listeMeteo = document.querySelector("#listeInfos");
+
+
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=marseille&appid=ebbef056fabd1f5c3932576f5181de0c';
+
+
+
+function meteoCity(nameRandom) {
+
+
+    
+    listeMeteo.innerHTML = '';
+
+    // Fetch: Requete HTTP - Fetch signifie " chercher "
+
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${nameRandom}&units=metric&lang=fr&appid=ebbef056fabd1f5c3932576f5181de0c`)
+        .then(Response => Response.json())
+        .then(data => {
+             
+            // Boucle "for" , tant que i est inférieur à 5, incrémentation de +1
+            for (let i = 0; i < 5; i++) {
+
+                listeMeteo.insertAdjacentHTML("beforeend", `<li><p>${data.list[i].dt_txt}, degrés: ${data.list[i].main.temp}°C</p></li>`)
+                // Sert à écrire dans la page HTML 
+            }
+
+            console.log(data)
+        })
+
+}
+
+button.addEventListener('click', () => {
+    meteoCity(input.value)
+});
+
